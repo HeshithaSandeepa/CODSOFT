ba14a119ad16ae03fb3885959c43390b99675e9a
Heshitha <hesithedg@gmail.com>
2023-12-19 13:10:34 +0530

    portfolio-update header section and about section
    *add about section  image
    *add styles

---
+++ Level-1/PORTFOLIO/index.js
@@ -0,0 +1,21 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// typed animation
+let typed = new Typed(" .typing",{
+    strings:[" Web Developer","App Developer","UI/UX Designer"],
+    typespeed:100,
+    backSpeed:60,
+    loop:true
+})
