15809d2925ba8a2f6d7127c06bcc91ed726ff01a
Heshitha <hesithedg@gmail.com>
2023-12-22 22:20:25 +0530

    add contact us page
    *add section contact-me
    *add styles contact-me section

--- Level-1/PORTFOLIO/index.js
+++ Level-1/PORTFOLIO/index.js
@@ -1,17 +1,3 @@
-//about me education skills
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
 
 //typed animation
 let typed = new Typed(" .typing",{
